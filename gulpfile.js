5135ae274c25ab9b980eaad057bb802ddac39f8c
Barbara Kopiec <bakopiec@gmail.com>
2018-10-26 21:17:38 +0200

    Add generating files stylesheet and javascript

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,24 @@
 var browserSync = require('browser-sync').create();
 var gulp = require('gulp');
+var gulpConcat = require('gulp-concat');
+var gulpSass = require('gulp-sass');
 
 var paths = {
     html: [
         'app/**/*.html'
+    ],
+    sass: [
+        'app/styles/style.scss'
+    ],
+    sassWatch: [
+        'app/styles/**/*.scss'
+    ],
+    js: [
+        'bower_components/jquery/dist/jquery.js',
+        'bower_components/bootstrap-sass/assets/javascripts/bootstrap.js'
+    ],
+    jsWatch: [
+        'app/scripts/**/*.js'
     ]
 }
 
@@ -12,8 +27,24 @@ gulp.task('html', function() {
             .pipe(gulp.dest('dist'));
 });
 
+gulp.task('sass', function() {
+    gulp.src(paths.sass)
+            .pipe(gulpSass())
+            .pipe(gulp.dest('dist'));
+});
+
+gulp.task('js', function() {
+    gulp.src(paths.js)
+            .pipe(gulpConcat('scripts.js'))
+            .pipe(gulp.dest('dist'));
+});
+
 gulp.task('watch', function() {
     gulp.watch(paths.html, ['html'])
+            .on('change', browserSync.reload);
+    gulp.watch(paths.sassWatch, ['sass'])
+            .on('change', browserSync.stream);
+    gulp.watch(paths.jsWatch, ['js'])
             .on('change', browserSync.reload);
 });
 
@@ -23,4 +54,4 @@ gulp.task('serve', function() {
     });
 });
 
-gulp.task('default', ['html', 'watch', 'serve']);
+gulp.task('default', ['html', 'sass', 'js', 'watch', 'serve']);
